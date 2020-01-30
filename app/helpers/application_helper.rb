82df44066d59a08be1f64fdafe9904888709a434
Brandon Weaver <a7xncob@gmail.com>
2020-01-30 18:04:08 +0000

    Automatically backed up by Learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
-  def last_updated(post)
-    return post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  def last_updated(object)
+    return object.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
   end
   
 end
